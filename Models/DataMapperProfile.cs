cab03efc998162b8646d86f58b21547ed1b37047
Arga Dinata <arga_dinata@outlook.com>
2023-09-20 18:07:51 +0700

    update: automap snake_case properties

--- Models/DataMapperProfile.cs
+++ Models/DataMapperProfile.cs
@@ -6,19 +6,16 @@ namespace CustomerDb.Models
     {
         public DataMapperProfile()
         {
+            SourceMemberNamingConvention = new LowerUnderscoreNamingConvention();
+            DestinationMemberNamingConvention = new LowerUnderscoreNamingConvention();
+
             // Company Map
             CreateMap<CompanyRequest, Company>();
             CreateMap<Company, CompanyResponse>();
 
             // Contact Map
             CreateMap<ContactRequest, Contact>();
-
-            CreateMap<Contact, ContactResponse>()
-                .ForMember(dest => dest.company_name, options => options.MapFrom(src => src.company == null ? null : src.company.name))
-                .ForMember(dest => dest.company_website, options => options.MapFrom(src => src.company == null ? null : src.company.website))
-                .ForMember(dest => dest.company_email, options => options.MapFrom(src => src.company == null ? null : src.company.email))
-                .ForMember(dest => dest.company_phone_no, options => options.MapFrom(src => src.company == null ? null : src.company.phone_no))
-                .ForMember(dest => dest.company_address, options => options.MapFrom(src => src.company == null ? null : src.company.address));
+            CreateMap<Contact, ContactResponse>();
         }
     }
 }
